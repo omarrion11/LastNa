fda1b3964d81345d6e2dc33f58bb3a9573b2a53f
virgil-aguilar <virgil-aguilar5@gmail.com>
2014-09-20 19:58:01 +0800

    Copy

---
+++ gen/ucu/edu/ccs/ie_dictionary/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ucu.edu.ccs.ie_dictionary;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ucu/edu/ccs/ie_dictionary/Wordsearch.java
@@ -0,0 +1,22 @@
+package ucu.edu.ccs.ie_dictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Wordsearch extends Activity{
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_item);
+	
+		
+
+}
+	
+
+	
+}
